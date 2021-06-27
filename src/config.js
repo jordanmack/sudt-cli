f83c59e034229c34c3a7f8ce70ae5b34e7c742fb
Jordan Mack <jordan.mack@brilliantnotion.com>
2021-06-26 23:35:08 -0700

    Fix pathing for compatibility with npm app installation.

--- src/config.js
+++ src/config.js
@@ -5,7 +5,7 @@ const config =
 		ckbRpcUrl: 'http://localhost:8114',
 		ckbIndexerUrl: 'http://localhost:8116',
 		ckbExplorerUrl: '',
-		configFile: './devnet-config.json',
+		configFile: __dirname+'/../devnet-config.json',
 		genesisCell1PrivateKey: '0xd00c06bfd800d27397002dca6fb0993d5ba6399b4238b2f29ee9deb97593d2bc',
 		genesisCell2PrivateKey: '0x63d86723e08f0f813a36ce6aa123bb2289d90680ae1e99d4de8cdb334553f24d',
 	},
@@ -23,8 +23,8 @@ const config =
 	},
 	assets:
 	{
-		pwLockScriptCodeBinary: './assets/binaries/pwlock',
-		sudtScriptCodeBinary: './assets/binaries/sudt',
+		pwLockScriptCodeBinary: __dirname+'/../assets/binaries/pwlock',
+		sudtScriptCodeBinary: __dirname+'/../assets/binaries/sudt',
 	}
 };
 
